832cddb4b982450af0de61a951803573f9a617a9
harryscholes <harryscholes@gmail.com>
2018-03-31 09:08:12 +0100

    Initial commit

--- src/FizzBuzz.jl
+++ src/FizzBuzz.jl
@@ -1,5 +1,91 @@
+__precompile__()
+
+"""
+Never Fail Another Interview™
+"""
 module FizzBuzz
 
-# package code goes here
+export fizzbuzz, @fizzbuzz
 
-end # module
+
+"""
+    fizzbuzz(iterable::AbstractArray)
+    fizzbuzz(iterable::AbstractArray, io:IO)
+
+FizzBuzz-ify an `iterable`. Optionally print to `io` instead of `STDOUT`.
+
+```jldoctest fb_env
+julia> using FizzBuzz
+
+julia> fizzbuzz(1:5)
+1
+2
+fizz
+4
+buzz
+
+julia> io = IOBuffer();
+
+julia> fizzbuzz(1:15, io)
+
+julia> String(take!(io))
+"1\\n2\\nfizz\\n4\\nbuzz\\nfizz\\n7\\n8\\nfizz\\nbuzz\\n11\\nfizz\\n13\\n14\\nfizz buzz\\n"
+```
+"""
+function fizzbuzz(iterable::T, io::Union{Base.TTY, IO}=STDOUT) where T<:AbstractArray{<:Real}
+    for i = iterable
+        println(io, fizzbuzzer(i))
+    end
+end
+
+
+"""
+Private functions that compute return values for elements of `iterable`.
+"""
+function fizzbuzzer(v::T)::Union{T, String} where T<:Real
+    if v % 15 == 0
+        return "fizz buzz"
+    elseif v % 5 == 0
+        return "buzz"
+    elseif v % 3 == 0
+        return "fizz"
+    else
+        return v
+    end
+end
+
+function fizzbuzzer(v::T)::Union{T, String} where T<:AbstractFloat
+    if v % 15. == 0.
+        return "fizz buzz"
+    elseif v % 5. == 0.
+        return "buzz"
+    elseif v % 3. == 0.
+        return "fizz"
+    else
+        return v
+    end
+end
+
+
+"""
+    @fizzbuzz iterable::AbstractArray
+    @fizzbuzz iterable::AbstractArray io::IO
+
+Macro version of `fizzbuzz()`.
+
+```jldoctest fb_env
+julia> @fizzbuzz 1:5
+1
+2
+fizz
+4
+buzz
+```
+"""
+macro fizzbuzz(iterable::Expr)
+    return quote
+        fizzbuzz($iterable)
+    end
+end
+
+end  # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,63 @@
 using FizzBuzz
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+const fizzbuzzer = FizzBuzz.fizzbuzzer
+const d = Dict(3 => "fizz", 5 => "buzz", 15 => "fizz buzz")
+
+@testset "FizzBuzz" begin
+
+    @testset "Test <:Integer returns $(d[v])" for v = [3, 5, 15]
+        @test fizzbuzzer(v) == d[v]
+    end
+
+    @testset "Test <:AbstractFloat returns $(d[v])" for v = [3., 5., 15.]
+        @test fizzbuzzer(v) == d[v]
+    end
+
+    @testset "Test <:Rational returns $(d[v])" for v = [3//1, 5//1, 15//1]
+        @test fizzbuzzer(v) == d[v]
+    end
+
+    @testset "Test $v returns $v" for v = (1, 1., 1//1)
+        @test fizzbuzzer(v) == v
+    end
+
+    @testset "Test UnitRange{<:Integer}" begin
+        io = IOBuffer()
+        fizzbuzz(1:15, io)
+        @test String(take!(io)) == "1\n2\nfizz\n4\nbuzz\nfizz\n7\n8\nfizz\nbuzz\n11\nfizz\n13\n14\nfizz buzz\n"
+        close(io)
+    end
+
+    @testset "Test UnitRange{<:AbstractFloat}" begin
+        io = IOBuffer()
+        fizzbuzz(1.:15., io)
+        @test String(take!(io)) == "1.0\n2.0\nfizz\n4.0\nbuzz\nfizz\n7.0\n8.0\nfizz\nbuzz\n11.0\nfizz\n13.0\n14.0\nfizz buzz\n"
+        close(io)
+    end
+
+    @testset "Test Array{<:Integer}" begin
+        io = IOBuffer()
+        fizzbuzz(collect(1:15), io)
+        @test String(take!(io)) == "1\n2\nfizz\n4\nbuzz\nfizz\n7\n8\nfizz\nbuzz\n11\nfizz\n13\n14\nfizz buzz\n"
+    end
+
+    @testset "Test Array{<:AbstractFloat}" begin
+        io = IOBuffer()
+        fizzbuzz(collect(1.:15.), io)
+        @test String(take!(io)) == "1.0\n2.0\nfizz\n4.0\nbuzz\nfizz\n7.0\n8.0\nfizz\nbuzz\n11.0\nfizz\n13.0\n14.0\nfizz buzz\n"
+    end
+
+    @testset "Test not divisible by 3 or 5 returns number" begin
+        for v = 1:100
+            while true
+                r = rand(1:100_000, 1)[1]
+                if r % 3 > 0 && r % 5 > 0
+                    @test fizzbuzzer(r) == r
+                    break
+                end
+            end
+        end
+    end
+
+end
